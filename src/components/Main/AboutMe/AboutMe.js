1a5c7aabf010f04e2cba4bcd7571b53a406401c7
WhatsAppIT <it.technology@internet.ru>
2024-01-04 22:54:42 +0300

    main

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './AboutMe.css';
+import userFoto from '../../../images/aboutfoto.svg';
 
 function AboutMe() {
-  return <h2>AboutMe</h2>;
+  return (
+    <section className='about'>
+      <h2 className='about__title'>Студент</h2>
+      <div className='about__info'>
+        <div className='about__info_img'>
+          <img src={userFoto} className='about__info_user_image' />
+        </div>
+        <div className='about__info_user'>
+          <p className='about__info_user_name'>Виталий</p>
+          <p className='about__info_user_job'>Фронтенд-разработчик, 30 лет</p>
+          <p className='about__info_user_about'>
+            Я родился и живу в Саратове, закончил факультет экономики СГУ. У
+            меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
+            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
+            Контур». После того, как прошёл курс по веб-разработке, начал
+            заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <p className='about__info_user_git'>Github</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutMe;
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import './Portfolio.css';
+import logoPortfolio from '../../../images/logoPortfolio.svg';
 
 function Portfolio() {
-  return <h2>Portfolio</h2>;
+  return (
+    <section className='portfolio'>
+      <h2 className='portfolio__title'>Портфолио</h2>
+      <div className='portfolio__static'>
+        <a href='#' className='portfolio__link portfolio__link_static'>
+          Статичный сайт
+        </a>
+        <img
+          src={logoPortfolio}
+          className='portfolio__logo portfolio__logo_static'
+          alt='ссылка'
+        />
+      </div>
+      <div className='portfolio__adaptiv'>
+        <a href='#' className='portfolio__link portfolio__link_adaptive'>
+          Адаптивный сайт
+        </a>
+        <img
+          src={logoPortfolio}
+          className='portfolio__logo portfolio__logo_adaptive'
+          alt='ссылка'
+        />
+      </div>
+      <div className='portfolio__spa'>
+        <a href='#' className='portfolio__link portfolio__link_spa'>
+          Одностраничное приложение
+        </a>
+        <img
+          src={logoPortfolio}
+          className='portfolio__logo portfolio__logo_spa'
+          alt='ссылка'
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
