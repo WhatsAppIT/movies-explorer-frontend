e7c2d0916c7dcc6d7d713359f26edf60dff8940f
WhatsAppIT <it.technology@internet.ru>
2024-01-14 11:03:19 +0300

    movies moviesCard/List

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,20 +1,48 @@
 import React from 'react';
-import img1 from '../../../images/pic__COLOR_pic.svg';
-import save from '../../../images/save.svg';
+//import img1 from '../../../images/pic__COLOR_pic.svg';
+//import save from '../../../images/save.svg';
 import './MoviesCard.css';
 
-function MoviesCard() {
+function MoviesCard(props) {
+  const {
+    card: { nameRU, duration, image },
+    save,
+  } = props;
+
+  const [isSaved, setIsSaved] = React.useState(false);
+
+  function handleSave() {
+    setIsSaved((saved) => !saved);
+  }
+
+  function durationMovie(duration) {
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+
+    if (hours < 1) {
+      return `${minutes} м`;
+    } else if (minutes === 0) {
+      return `${hours} ч`;
+    } else {
+      return `${hours} ч ${minutes} м`;
+    }
+  }
+
   return (
     <div className='card'>
       <div className='card__image'>
-        <img className='card__image_foto' src={img1} alt='фото фильма' />
-        <button type='button' className='card__image_save'>
-          <img src={save} />
-        </button>
+        <img className='card__image_foto' src={image} alt={nameRU} />
+        <button
+          type='button'
+          className={`card__image_save ${
+            isSaved ? 'card__image_save_true' : 'card__image_save_false'
+          }`}
+          onClick={handleSave}
+        ></button>
       </div>
       <div className='card__info'>
-        <div className='card__info_title'>33 слова о дизайне</div>
-        <div className='card__info_time'>1ч 17м</div>
+        <h2 className='card__info_title'>{nameRU}</h2>
+        <div className='card__info_time'>{durationMovie(duration)}</div>
       </div>
     </div>
   );
